cb147612ab73f01224fd4def5bf35486b6e19d4f
Parantapa Bhattacharya <pb@parantapa.net>
2020-08-22 20:01:01 -0400

    Add calc

--- src/pbcmd/cli.py
+++ src/pbcmd/cli.py
@@ -1,10 +1,11 @@
-"""CLI interface."""
+"""CLI interface. """
 
 import click_completion
 
 from . import cli
 
 import pbcmd.hello
+import pbcmd.calc
 
 if __name__ == "__main__":
     click_completion.init()
