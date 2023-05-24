45bccb39370833f0bd49a08e61ba457259bd6971
Parantapa Bhattacharya <pb@parantapa.net>
2023-05-24 10:30:24 -0400

    csplit: add support for splitting csv files

--- src/pbcmd/csplit.py
+++ src/pbcmd/csplit.py
@@ -54,6 +54,12 @@ def infer_compression(fname: str) -> str:
     type=COMP_OPTION_TYPE,
     help="Compression format of output files",
 )
+@click.option(
+    "-h",
+    "--keep-header-line",
+    is_flag=True,
+    help="Keep header line",
+)
 @click.argument("input-file")
 @click.argument("output-file-format")
 def csplit(
@@ -61,6 +67,7 @@ def csplit(
     encoding,
     input_compression,
     output_compression,
+    keep_header_line,
     input_file,
     output_file_format,
 ):
@@ -85,6 +92,11 @@ def csplit(
         fin = iter(fin)
         index = 0
 
+        if keep_header_line:
+            header = next(fin)
+        else:
+            header = None
+
         obar = tqdm(desc="Writing file")
         while True:
             lines = list(islice(fin, lines_per_file))
@@ -96,6 +108,8 @@ def csplit(
 
             output_file = output_file_format.format(index=index)
             with out_open(output_file, mode="wt", encoding=encoding) as fout:
+                if header is not None:
+                    fout.write(header)
                 for line in ibar:
                     fout.write(line)
             index += 1
